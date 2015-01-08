556f19445e9b2991a0df561a2ac5f00917ab12f9
bioshrek <shrekwang1@gmail.com>
2015-01-08 15:18:33 +0800

    Initial Commit

---
+++ SKTwitterViewControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SKTwitterViewControllerDemo
+//
+//  Created by Shrek Wang on 1/8/15.
+//  Copyright (c) 2015 Shrek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SKTwitterViewControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SKTwitterViewControllerDemo
+//
+//  Created by Shrek Wang on 1/8/15.
+//  Copyright (c) 2015 Shrek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
